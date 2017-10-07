9207598302cfd251e26e793e5a1656723cfa22e8
Peter Chiu <peterchiushunhang@hotmail.com>
2017-10-07 15:31:32 +1300

    Built basic list view for songs

--- app/src/main/java/com/noahark/noaharkplayer/MainActivity.java
+++ app/src/main/java/com/noahark/noaharkplayer/MainActivity.java
@@ -1,13 +1,39 @@
 package com.noahark.noaharkplayer;
 
 import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] songs = {"Song A", "Song B", "Song C", "Song D", "Song E", "Song F"};
+        ListAdapter musicListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, songs);
+        ListView lvMusicList = (ListView) findViewById(R.id.lvMusicList);
+        lvMusicList.setAdapter(musicListAdapter);
+
+        lvMusicList.setOnItemClickListener(
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String song = String.valueOf(parent.getItemAtPosition(position));
+                        Toast.makeText(MainActivity.this, song, Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
+
 }
