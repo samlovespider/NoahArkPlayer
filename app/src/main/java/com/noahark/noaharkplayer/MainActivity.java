f679d2641d6e94077308780d284a3411dcc05de5
Peter Chiu <peterchiushunhang@hotmail.com>
2017-10-09 17:47:07 +1300

    Created two line linear layout for activity_main

--- app/src/main/java/com/noahark/noaharkplayer/MainActivity.java
+++ app/src/main/java/com/noahark/noaharkplayer/MainActivity.java
@@ -10,11 +10,16 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class MainActivity extends Activity {
 
+/*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,5 +40,54 @@ public class MainActivity extends Activity {
                 }
         );
     }
+*/
 
 }
+
+/*
+public class MainActivity extends AppCompatActivity {
+    ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+    private SimpleAdapter sa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        HashMap<String, String> item;
+        for (int i = 0; i < SongAndAuthor.length; i++) {
+            item = new HashMap<String, String>();
+            item.put("line1", SongAndAuthor[i][0]);
+            item.put("line2", SongAndAuthor[i][1]);
+            list.add(item);
+        }
+        sa = new SimpleAdapter(this, list,
+                R.layout.activity_main,
+                new String[]{"line1", "line2"},
+                new int[]{R.id.tvSongTitle, R.id.tvAlbumTitle});
+        ((ListView) findViewById(R.id.lvMusicList)).setAdapter(sa);
+    }
+
+    private String[][] SongAndAuthor =
+            {
+                    {"Alabama", "Montgomery"},
+                    {"Alaska", "Juneau"},
+                    {"Arizona", "Phoenix"},
+                    {"Arkansas", "Little Rock"},
+                    {"California","Sacramento"},
+                    {"Colorado","Denver"},
+                    {"Connecticut","Hartford"},
+                    {"Delaware","Dover"},
+                    {"Florida","Tallahassee"},
+                    {"Georgia","Atlanta"},
+                    {"Hawaii","Honolulu"},
+                    {"Idaho","Boise"},
+                    {"Illinois","Springfield"},
+                    {"Indiana","Indianapolis"},
+                    {"Iowa","Des Moines"},
+                    {"Kansas","Topeka"},
+                    {"Kentucky","Frankfort"},
+                    {"Louisiana","Baton Rouge"},
+                    {"Maine","Augusta"}
+            };
+
+    }*/
