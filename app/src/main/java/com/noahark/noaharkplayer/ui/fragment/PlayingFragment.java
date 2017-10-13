bef6560906cffa29216b640b39b526a8bd45f69a
Frank <yuandahuo@gmail.com>
2017-10-13 13:52:14 +1300

    Finish repeat and shuffle bar

--- app/src/main/java/com/noahark/noaharkplayer/ui/fragment/PlayingFragment.java
+++ app/src/main/java/com/noahark/noaharkplayer/ui/fragment/PlayingFragment.java
@@ -1,9 +1,13 @@
 package com.noahark.noaharkplayer.ui.fragment;
 
 
+import android.view.View;
+import android.widget.Toast;
+
 import com.noahark.noaharkplayer.R;
 import com.noahark.noaharkplayer.base.ui.BaseFragment;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 
 @EFragment(R.layout.fragment_playing)
@@ -13,4 +17,44 @@ public class PlayingFragment extends BaseFragment {
     public void initView() {
 
     }
+
+
+    private boolean isPlaying = false;
+
+    @Click({R.id.ib_favorite,R.id.ib_repeat,R.id.ib_shuffle})
+    @Override
+    public void initClick(View view) {
+        Toast.makeText(getContext(), "play:" + isPlaying, Toast.LENGTH_SHORT).show();
+
+        switch (view.getId()) {
+            case R.id.ib_repeat:
+                if (isPlaying) {
+                    view.setBackgroundResource(R.drawable.ic_music_repeat_normal);
+                    isPlaying = false;
+                } else {
+                    view.setBackgroundResource(R.drawable.ic_music_repeat_pressed);
+                    isPlaying = true;
+                }
+                break;
+            case R.id.ib_favorite:
+                if (isPlaying) {
+                    view.setBackgroundResource(R.drawable.ic_music_favorite_normal);
+                    isPlaying = false;
+                } else {
+                    view.setBackgroundResource(R.drawable.ic_music_favorite_pressed);
+                    isPlaying = true;
+                }
+                break;
+            case R.id.ib_shuffle:
+                if (isPlaying) {
+                    view.setBackgroundResource(R.drawable.ic_music_shuffle_normal);
+                    isPlaying = false;
+                } else {
+                    view.setBackgroundResource(R.drawable.ic_music_shuffle_pressed);
+                    isPlaying = true;
+                }
+                break;
+        }
+    }
+
 }
