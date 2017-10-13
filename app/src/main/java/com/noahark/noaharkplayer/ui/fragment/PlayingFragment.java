16ae8fc392004d4efb55732124f684d1628b45da
Frank <yuandahuo@gmail.com>
2017-10-13 15:46:51 +1300

    Finished repeat, shuffle and favorite button

--- app/src/main/java/com/noahark/noaharkplayer/ui/fragment/PlayingFragment.java
+++ app/src/main/java/com/noahark/noaharkplayer/ui/fragment/PlayingFragment.java
@@ -10,6 +10,8 @@ import com.noahark.noaharkplayer.base.ui.BaseFragment;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 
+import java.io.InterruptedIOException;
+
 @EFragment(R.layout.fragment_playing)
 public class PlayingFragment extends BaseFragment {
 
@@ -19,39 +21,45 @@ public class PlayingFragment extends BaseFragment {
     }
 
 
-    private boolean isPlaying = false;
-
+    private boolean isfavorite = false;
+    private boolean isSuffle =false;
+    private int i=0;
     @Click({R.id.ib_favorite,R.id.ib_repeat,R.id.ib_shuffle})
     @Override
     public void initClick(View view) {
-        Toast.makeText(getContext(), "play:" + isPlaying, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getContext(), "play:" + isPlaying, Toast.LENGTH_SHORT).show();
 
         switch (view.getId()) {
             case R.id.ib_repeat:
-                if (isPlaying) {
-                    view.setBackgroundResource(R.drawable.ic_music_repeat_normal);
-                    isPlaying = false;
-                } else {
-                    view.setBackgroundResource(R.drawable.ic_music_repeat_pressed);
-                    isPlaying = true;
-                }
+                    if (i == 0) {
+                        view.setBackgroundResource(R.drawable.ic_music_repeat_normal);
+                        i++;
+                    }else
+                    if (i == 1) {
+                        view.setBackgroundResource(R.drawable.ic_music_repeat_pressed);
+                        i++;
+                    }else
+                    if (i == 2) {
+                        view.setBackgroundResource(R.drawable.ic_music_repeat_one_pressed);
+                        i = 0;
+                    }
                 break;
             case R.id.ib_favorite:
-                if (isPlaying) {
+                if (isfavorite) {
                     view.setBackgroundResource(R.drawable.ic_music_favorite_normal);
-                    isPlaying = false;
+                    isfavorite = false;
                 } else {
                     view.setBackgroundResource(R.drawable.ic_music_favorite_pressed);
-                    isPlaying = true;
+                    isfavorite = true;
                 }
                 break;
             case R.id.ib_shuffle:
-                if (isPlaying) {
+                if (isSuffle) {
                     view.setBackgroundResource(R.drawable.ic_music_shuffle_normal);
-                    isPlaying = false;
+                    isSuffle = false;
                 } else {
                     view.setBackgroundResource(R.drawable.ic_music_shuffle_pressed);
-                    isPlaying = true;
+                    isSuffle = true;
                 }
                 break;
         }
